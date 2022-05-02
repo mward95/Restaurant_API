288282785d24225bc43e2de192f9e26f9e0b6543
Matthew Ward <wardmatt95@gmail.com>
2022-05-02 17:45:47 -0400

    Final Clean ups

--- Matthew Ward/config.py
+++
@@ -1,8 +0,0 @@
-# Enter your API keys
-
-# OpenWeatherMap API Key
-weather_api_key =""
-
-
-# Google API Key
-gkey =""
---
+++ Restaurant Data/config.py
@@ -0,0 +1,8 @@
+# Enter your API keys
+
+# OpenWeatherMap API Key
+weather_api_key ="2e616d4a7c02f76336a5035c00e358ce"
+
+
+# Google API Key
+gkey ="AIzaSyCt3iZn02ZXST9uTFioz5cxC4wp5KiXLK0"
